7925391754f5948930a7dca5fc49d47f751d1a81
AmenaTalukder <amenaakter9714@gmail.com>
2026-02-04 02:36:42 +0500

    done

--- sub-components/services/Frontent/Frontend.jsx
+++ sub-components/services/Frontent/Frontend.jsx
@@ -5,6 +5,7 @@ import Travel from "../../../src/assets/Travel App.jpg";
 import Transaction from "../../../src/assets/Transaction-Dashboard.jpg";
 import FreshFood from "../../../src/assets/Fresh-Food.jpg";
 import LearnFlow from "../../../src/assets/learnFlow.jpg";
+import HotelManagement from "../../../src/assets/HotelManagement.png"
 
 const Frontend = () => {
   const frontendProjects = [
