7e489d4403ceee23f9a91bd973dc551d81be6bcb
AmenaTalukder <amenaakter9714@gmail.com>
2026-02-04 00:39:33 +0500

    done

--- sub-components/services/Frontent/Frontend.jsx
+++ sub-components/services/Frontent/Frontend.jsx
@@ -65,15 +65,7 @@ const Frontend = () => {
       link: "https://online-education-nine-tau.vercel.app/",
       featured: true
     },
-    // {
-    //   title: "E-Commerce Platform",
-    //   description:
-    //     "E-commerce application with payment integration and Seller Dashboard.",
-    //   image: QuickCart,
-    //   technologies: ["React", "Node.js", "MongoDB", "Clerk", "Cloudinary"],
-    //   link: "https://quickcart-ecommerce-psi.vercel.app/",
-    //   featured: true
-    // }
+  
   ];
 
   return (
